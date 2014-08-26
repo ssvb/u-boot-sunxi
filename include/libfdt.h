0dd590537ad6a75a15122fac0a5f5aa088defb12
Thierry Reding <treding@nvidia.com>
2014-08-26 17:33:51 +0200

    fdt: Add a function to get the index of a string
    Given a device tree node and a property name, the new fdt_find_string()
    function will look up a given string in the string list contained in the
    property's value and return its index.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Acked-by: Simon Glass <sjg@chromium.org>

--- include/libfdt.h
+++ include/libfdt.h
@@ -857,6 +857,17 @@ int fdt_node_offset_by_compatible(const void *fdt, int startoffset,
  */
 int fdt_stringlist_contains(const char *strlist, int listlen, const char *str);
 
+/**
+ * fdt_find_string - find a string in a string list and return its index
+ * @fdt: pointer to the device tree blob
+ * @node: offset of the node
+ * @property: name of the property containing the string list
+ * @string: string to look up in the string list
+ * @return: the index of the string or negative on error
+ */
+int fdt_find_string(const void *fdt, int node, const char *property,
+		    const char *string);
+
 /**********************************************************************/
 /* Read-only functions (addressing related)                           */
 /**********************************************************************/
